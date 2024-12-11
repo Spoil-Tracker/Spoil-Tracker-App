07f6f059e86c5b84a6e15cacc70489f221029b74
TheThirdPlayerFromTheLeft <souleater142356@gmail.com>
2024-12-10 20:57:07 -0800

    Small name changes  + notes update

--- spoil-tracker-alpha/app/index.tsx
+++ spoil-tracker-alpha/app/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Import from 'react-dom/client' in React 18
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import App2 from "../src/App2"; // Your main app component
+import App2 from "../src/BackendDemo"; // Your main app component
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql", // Replace with your backend URL
