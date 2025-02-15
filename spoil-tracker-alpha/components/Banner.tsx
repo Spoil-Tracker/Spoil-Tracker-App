01609bec67478ff51179bb2ea17cec1b11dbe1c7
Rex Tabora <rex.tabora@yahoo.com>
2025-02-15 01:43:38 -0800

    Revert banner changes

--- spoil-tracker-alpha/components/Banner.tsx
+++ spoil-tracker-alpha/components/Banner.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { AntDesign } from '@expo/vector-icons'; // Ensure you have @expo/vector-icons installed
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 interface BannerProps {
   message: string;
@@ -8,10 +7,6 @@ interface BannerProps {
 }
 
 const Banner: React.FC<BannerProps> = ({ message, type }) => {
-  const [visible, setVisible] = useState(true);
-
-  if (!visible) return null; // Hide banner when dismissed
-
   return (
     <View
       style={[
@@ -20,9 +15,6 @@ const Banner: React.FC<BannerProps> = ({ message, type }) => {
       ]}
     >
       <Text style={styles.bannerText}>{message}</Text>
-      <TouchableOpacity onPress={() => setVisible(false)} style={styles.closeButton}>
-        <AntDesign name="close" size={20} color="white" />
-      </TouchableOpacity>
     </View>
   );
 };
@@ -30,15 +22,10 @@ const Banner: React.FC<BannerProps> = ({ message, type }) => {
 const styles = StyleSheet.create({
   banner: {
     padding: 15,
-    margin: 15, // Ensure some space around the banner
+    margin: 10,
     borderRadius: 5,
-    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    position: 'absolute',
-    justifyContent: 'space-between', // Space between text and close button
-    top: 20, // Adjust as needed
-    left: 10,
-    right: 10,
   },
   successBanner: {
     backgroundColor: '#28a745',
@@ -49,11 +36,6 @@ const styles = StyleSheet.create({
   bannerText: {
     color: 'white',
     fontWeight: 'bold',
-    flex: 1, // Allows text to take available space
-  },
-  closeButton: {
-    marginLeft: 10, // Add some spacing between text and button
-    padding: 5,
   },
 });
 
